07383bb06a4fa8332482c097572883a608f0a4ad
Ovidio R. Calvet <calveto@gmail.com>
2018-05-13 19:52:25 -0400

    refactor to create an app package

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/ocalvet/tello-testing/app"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/api"
 	"gobot.io/x/gobot/platforms/dji/tello"
@@ -15,104 +13,8 @@ func main() {
 	master := gobot.NewMaster()
 	a := api.NewAPI(master)
 	a.Start()
-	// work := func() {
-	// 	drone.TakeOff()
-
-	// 	gobot.After(5*time.Second, func() {
-	// 		drone.FrontFlip()
-	// 	})
-
-	// 	gobot.After(10*time.Second, func() {
-	// 		drone.BackFlip()
-	// 	})
-
-	// 	gobot.After(15*time.Second, func() {
-	// 		drone.Land()
-	// 	})
-	// }
-
-	work := func() {
-		drone.TakeOff()
-		r := gin.Default()
-		r.Use(cors.New(cors.Config{
-			AllowAllOrigins:  true,
-			AllowCredentials: true,
-			AllowMethods:     []string{"*"},
-			AllowHeaders:     []string{"*"},
-		}))
-		r.Use()
-		r.GET("/tello/takeoff", func(c *gin.Context) {
-			drone.TakeOff()
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-			})
-		})
-
-		r.GET("/tello/land", func(c *gin.Context) {
-			drone.Land()
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-			})
-		})
-
-		r.GET("/tello/left", func(c *gin.Context) {
-			drone.Left(1)
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-			})
-		})
-
-		r.GET("/tello/right", func(c *gin.Context) {
-			drone.Right(1)
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-			})
-		})
-
-		r.GET("/tello/forward", func(c *gin.Context) {
-			drone.Forward(1)
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-			})
-		})
-
-		r.GET("/tello/backward", func(c *gin.Context) {
-			drone.Backward(1)
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-			})
-		})
-
-		r.GET("/tello/flip/right", func(c *gin.Context) {
-			drone.RightFlip()
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-			})
-		})
-
-		r.GET("/tello/flip/left", func(c *gin.Context) {
-			drone.LeftFlip()
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-			})
-		})
-
-		r.GET("/tello/flip/front", func(c *gin.Context) {
-			drone.FrontFlip()
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-			})
-		})
-
-		r.GET("/tello/flip/back", func(c *gin.Context) {
-			drone.BackFlip()
-			c.JSON(http.StatusOK, gin.H{
-				"success": true,
-			})
-		})
-
-		r.Run(":9081")
-	}
+	r := gin.Default()
+	work := app.CreateWorker(drone, r)
 	robot := gobot.NewRobot("tello",
 		[]gobot.Connection{},
 		[]gobot.Device{drone},
